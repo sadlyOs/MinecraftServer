4b523751959b463d35acecb20df7d5ef7f38f7bb
sadlyOs <jukovkill@gmail.com>
2025-11-05 01:26:06 +0500

    add MainFilterSection

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/sections/HeroSection/HeroSection"
+import MainFilterSection from "@/components/sections/MainFilterSection/MainFilterSection"
 
 export default function Home() {
     return (
         <main>
             <HeroSection />
+            <MainFilterSection />
         </main>
     )
 }
---
+++ src/components/sections/MainFilterSection/MainFilterSection.jsx
@@ -0,0 +1,7 @@
+export default function MainFilterSection() {
+    return (
+        <section>
+            <p>MainFilterSection</p>
+        </section>
+    )
+}
