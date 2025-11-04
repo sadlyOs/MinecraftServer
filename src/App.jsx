5b636da2e9516a4d5a2dd60468aeba247187359f
sadlyOs <jukovkill@gmail.com>
2025-11-04 23:46:26 +0500

    finish header desktop

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <>
-      <Header />
       <BrowserRouter>
+      <Header />
         <Routes>
           <Route path='/' element={<Home />}/>
         </Routes>
--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,7 +1,48 @@
+import icon from "@assets/header/icon.png"
+import logo from "@assets/header/logo.png"
+import { NavLink } from "react-router-dom"
+import './Header.css'
+import { useState } from "react"
+
 export default function Header() {
+
+    const [burger, setBurger] = useState(false)
+
     return (
-        <header>
-            <p>Header</p>
+        <header className="text-white bg-black">
+            <div className="container mx-auto p-4">
+                <div className="flex justify-between items-center">
+                    <div>
+                        <NavLink to='/'>
+                            <img src={logo} alt="logo" />
+                        </NavLink>
+                    </div>
+                    <div>
+                        <nav className="hidden md:block">
+                            <ul className="flex gap-7 text-sm font-light">
+                                {[['Скачать Minecraft', '/test1'], ['Создать сервер', '/test2'], ['Хостинг', '/test3'], ['Моды', '/test4']].map((item, index) => (
+                                    <NavLink key={index} to={item[1]} className="duration-100 hover:text-gray-400">{item[0]}</NavLink>
+                                ))}
+                            </ul>
+                        </nav>
+                    </div>
+                    <div>
+                        <div className="hidden md:block">
+                            <img src={icon} alt="log icon" />
+                        </div>
+                        <div onClick={() => setBurger(!burger)} className={"md:hidden burger " + (burger ? "active": "")}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {burger && (
+                <div>
+
+                </div>
+            )}
         </header>
     )
 }
