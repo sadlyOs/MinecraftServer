e5960bbc9fa29fa32a423d8ba5fe759032551db0
sadlyOs <jukovkill@gmail.com>
2025-11-05 02:47:37 +0500

    add star.svg to serverBlock

--- src/components/sections/ServerSection/ServerSection.jsx
+++ src/components/sections/ServerSection/ServerSection.jsx
@@ -1,8 +1,21 @@
+import serverImg from "@assets/serverBlock/serverImg1.png"
+
 export default function ServerSection() {
     return (
         <section className="container mx-auto px-4">
             <div className="flex justify-center">
-                <p>ServerSection</p>
+                <div className="rounded-2xl">
+                    <div>
+                        <img src={serverImg} alt="serverImg" className="rounded-t-2xl" />
+                    </div>
+                    <div>
+                        <div>
+                            <h3>🔥MINELANDY🔥</h3>
+                        </div>
+                    </div>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
         </section>
     )
