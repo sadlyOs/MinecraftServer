27ea72f95d4371efd11fbb978b200e24c6053361
sadlyOs <jukovkill@gmail.com>
2025-11-05 04:04:19 +0500

    add animate to MainFilter

--- src/components/sections/MainFilterSection/MainFilterSection.jsx
+++ src/components/sections/MainFilterSection/MainFilterSection.jsx
@@ -1,5 +1,11 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import MainFilterItem from "@/components/ui/MainFilterItem/MainFilterItem"
+import { useGSAP } from "@gsap/react"
+import ScrollTrigger from "gsap/src/ScrollTrigger"
+import gsap from "gsap"
+
+gsap.registerPlugin(useGSAP)
+gsap.registerPlugin(ScrollTrigger)
 
 export default function MainFilterSection() {
     const [version, setVersion] = useState(null)
@@ -7,8 +13,24 @@ export default function MainFilterSection() {
     const [miniGame, setMiniGame] = useState(null)
     const [mode, setMode] = useState(null)
     const [plugins, setPlugins] = useState(null)
+    const blockRef = useRef(null)
+
+    useGSAP(() => {
+        // Анимация элемента
+        gsap.from(blockRef.current, {
+        opacity: 0, // Начинаем с прозрачного
+        y: 50,      // Смещаем на 50px вниз
+        scrollTrigger: {
+            trigger: blockRef.current, // Элемент, который запускает анимацию
+            start: 'top 80%',         // Анимация начнется, когда верх элемента достигнет 80% высоты вьюпорта
+            end: 'bottom 20%',        // Анимация закончится, когда низ элемента достигнет 20% высоты вьюпорта
+                             // Анимация будет плавно следовать за прокруткой
+        }
+        });
+    })
+
     return (
-        <section className="container mx-auto py-5">
+        <section className="container mx-auto py-5" ref={blockRef}>
             <div className="flex flex-wrap justify-center gap-1.5 text-[0.8rem] md:text-[1rem]">
                 <MainFilterItem value={version} defaultValue={"Версия игры"} setValue={setVersion}/>
                 <MainFilterItem value={gameType} defaultValue={"Режимы"} setValue={setGameType}/>
