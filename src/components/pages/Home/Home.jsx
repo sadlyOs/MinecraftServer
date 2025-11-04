25f18c4f7047029a40440f2a1978d80a068997f3
sadlyOs <jukovkill@gmail.com>
2025-11-05 00:11:01 +0500

    add HeroSection to home

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
+import HeroSection from "@/components/sections/HeroSection/HeroSection"
+
 export default function Home() {
     return (
         <main>
-            <p>Home</p>
+            <HeroSection />
         </main>
     )
 }
