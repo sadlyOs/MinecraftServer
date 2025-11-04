ed2bb9cf66cc3da20ee0650d342c41984f0af41f
sadlyOs <jukovkill@gmail.com>
2025-11-05 01:29:13 +0500

    add arrow.svg

--- src/components/sections/MainFilterSection/MainFilterSection.jsx
+++ src/components/sections/MainFilterSection/MainFilterSection.jsx
@@ -1,7 +1,9 @@
 export default function MainFilterSection() {
     return (
-        <section>
-            <p>MainFilterSection</p>
+        <section className="container mx-auto py-5">
+            <div className="flex justify-center">
+                <p>test</p>
+            </div>
         </section>
     )
 }
