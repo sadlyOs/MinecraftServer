6082fe65a116afbd200e77afc9423d75d3488c63
sadlyOs <jukovkill@gmail.com>
2025-11-05 01:00:29 +0500

    edit size text HeroSection

--- src/components/sections/HeroSection/HeroSection.jsx
+++ src/components/sections/HeroSection/HeroSection.jsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
             <div className="text-white px-4 w-full h-full flex justify-center items-center bg-black-transparent">
                 <div className="text-center">
                     <h1 className="text-2xl md:text-4xl font-bold pb-2">Лучшие сервера Minecraft</h1>
-                    <p className="text-[0.7rem] md:text-[1.2rem]">Лучший рейтинг и мониторинг Майнкрафт-серверов в России.<br />Ищи топовые сервера с мини-играми, модами и плагинами,<br /> читай отзывы и следи за онлайном.</p>
+                    <p className="text-[0.6rem] md:text-[1.2rem]">Лучший рейтинг и мониторинг Майнкрафт-серверов в России.<br />Ищи топовые сервера с мини-играми, модами и плагинами,<br /> читай отзывы и следи за онлайном.</p>
                 </div>
             </div>
         </section>
