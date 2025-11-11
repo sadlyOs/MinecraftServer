f29ac588c6e0c77836643426b8a12c121c6bf01b
sadlyOs <jukovkill@gmail.com>
2025-11-11 13:10:52 +0500

    edit account menu

--- src/components/pages/AccountPage/AccountPage.jsx
+++ src/components/pages/AccountPage/AccountPage.jsx
@@ -15,7 +15,7 @@ export default function AccountPage() {
                             <ul className="flex justify-between lg:max-w-[1400px] max-w-full mx-auto overflow-x-scroll gap-4 px-4 py-6 scrollbar-hide snap-x snap-mandatory">
                                 {[['Акаунт', '/account/'], ['Мои сервера', '/account/servers/'], ['Добавить сервер', '/account/add/'], ["Создать проект", '/account/create/'], ['Мои проекты', '/account/myprojects/'], ["Пополнить баланс", "/account/balance/"], ['Мои платежи', '/account/mypayments']].map((item, index) => (
                                     <li key={index} className="flex-none snap-start">
-                                        <NavLink key={index} to={item[1]} className={({isActive}) => isActive ? "px-6 py-3 bg-blue-600 duration-300 rounded-full hover:bg-gray-main": "px-6 py-3 bg-gray-transparent duration-300 rounded-full hover:bg-gray-main"}>{item[0]}</NavLink>
+                                        <NavLink key={index} to={item[1]} className={({isActive}) => isActive ? "first:ml-4 px-6 py-3 bg-blue-600 duration-300 rounded-full hover:bg-gray-main": "px-6 py-3 bg-gray-transparent duration-300 rounded-full hover:bg-gray-main"}>{item[0]}</NavLink>
                                     </li>
                                 ))}
                             </ul>
