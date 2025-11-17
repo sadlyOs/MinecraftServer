c1f712feb7f5f70662329031d2ed91df290436d7
sadlyOs <jukovkill@gmail.com>
2025-11-17 19:12:40 +0500

    edit modal promo and modal color

--- src/components/ui/ModalColor/ModalColor.jsx
+++ src/components/ui/ModalColor/ModalColor.jsx
@@ -43,7 +43,7 @@ export default function ModalColor({ isOpen, onClose }) {
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
                     transition={{ duration: 0.4 }}
-                    className="fixed inset-0 w-full h-full flex justify-center items-center bg-black-transparent px-4 py-20 md:py-0 z-500"
+                    className="fixed inset-0 w-full h-full flex justify-center items-center bg-black-transparent px-4 py-20 lg:py-0 z-500"
                 >
                     <m.form
                         onSubmit={(e) => handleSubmit(e)}
@@ -52,89 +52,93 @@ export default function ModalColor({ isOpen, onClose }) {
                         animate={{ scale: 1, opacity: 1 }}
                         exit={{ scale: 0.8, opacity: 0 }}
                         transition={{ duration: 1, type: "spring" }}
-                        className="w-[450px] md:h-fit h-full overflow-y-scroll md:overscroll-none bg-modal-bg text-white relative p-5 box-border rounded-2xl backdrop-blur-2xl"
+                        className="w-[450px] lg:h-fit h-full overflow-y-scroll md:overscroll-none bg-modal-bg text-white relative lg:p-5 box-border rounded-2xl backdrop-blur-2xl"
                     >
-                        <div className="relative pb-4">
-                            <h3 className="text-center font-semibold">Выделить сервер цветом</h3>
-                            <div className="absolute right-0 top-1/2 -translate-y-full">
-                                <img onClick={() => { onClose() }} className="cursor-pointer duration-100 hover:scale-130" src={cancel} alt="cancel" />
+                        <div className="bg-modal-bg sticky top-0 pb-4 z-100 pt-5 lg:pt-0">
+                            <div className="relative">
+                                <h3 className="text-center font-semibold">Выделить сервер цветом</h3>
+                                <div className="absolute right-4 lg:right-0 top-1/2 -translate-y-full">
+                                    <img onClick={() => { onClose() }} className="cursor-pointer duration-100 hover:scale-130" src={cancel} alt="cancel" />
+                                </div>
                             </div>
                         </div>
-                        <div className="relative">
-                            <div>
-                                <Input handleClick={() => setIsArrayVersionOpen(!isArrayVersionOpen)} labelText={"Выберите или добавьте сервер для раскрутки"} style={"cursor-pointer"} placeholder={selectedVersion} readOnly={true} />
-                                <img src={selector} alt="selector" className="absolute right-4 md:top-14 top-20" />
+                        <div className="px-5 pb-5 lg:pb-0 lg:px-0">
+                            <div className="relative">
+                                <div>
+                                    <Input handleClick={() => setIsArrayVersionOpen(!isArrayVersionOpen)} labelText={"Выберите или добавьте сервер для раскрутки"} style={"cursor-pointer"} placeholder={selectedVersion} readOnly={true} />
+                                    <img src={selector} alt="selector" className="absolute right-4 md:top-14 top-20" />
+                                </div>
+                                <AnimatePresence>
+                                    {isArrayVersionOpen && (
+                                        <m.div
+                                            initial={{ opacity: 0, y: -50 }}
+                                            animate={{ opacity: 1, y: 0 }}
+                                            exit={{ opacity: 0, y: -50 }}
+                                            transition={{ duration: 0.3 }}
+                                            className="absolute z-10 top-full left-0 w-full max-h-60 overflow-y-auto bg-gray-transparent backdrop-blur-2xl rounded-2xl border-2 border-modal">
+                                            {arrayVersion.map((version, index) => (
+                                                <div key={index} onClick={() => { setIsArrayVersionOpen(false); setSelectedVersion(version) }} className="px-4 py-3 cursor-pointer bg-modal-bg hover:bg-gray-main duration-100">
+                                                    {version}
+                                                </div>
+                                            ))}
+                                        </m.div>
+                                    )}
+                                </AnimatePresence>
                             </div>
-                            <AnimatePresence>
-                                {isArrayVersionOpen && (
-                                    <m.div
-                                        initial={{ opacity: 0, y: -50 }}
-                                        animate={{ opacity: 1, y: 0 }}
-                                        exit={{ opacity: 0, y: -50 }}
-                                        transition={{ duration: 0.3 }}
-                                        className="absolute z-10 top-[110%] left-0 w-full max-h-60 overflow-y-auto bg-gray-transparent backdrop-blur-2xl rounded-2xl border-2 border-modal">
-                                        {arrayVersion.map((version, index) => (
-                                            <div key={index} onClick={() => { setIsArrayVersionOpen(false); setSelectedVersion(version) }} className="px-4 py-3 cursor-pointer bg-modal-bg hover:bg-gray-main duration-100">
-                                                {version}
-                                            </div>
-                                        ))}
-                                    </m.div>
-                                )}
-                            </AnimatePresence>
-                        </div>
-                        <div className="py-5">
-                            <div>
-                                <p className="pb-2 text-gray-main">Выберите цвет выделения сервера:</p>
-                            </div>
-                            <div className="flex flex-col gap-3 p-4">
-                                {colors.map((color, index) => {
-                                    const isSelected = selectedIndex === index;
+                            <div className="py-5">
+                                <div>
+                                    <p className="pb-2 text-gray-main">Выберите цвет выделения сервера:</p>
+                                </div>
+                                <div className="flex flex-col gap-3 p-4">
+                                    {colors.map((color, index) => {
+                                        const isSelected = selectedIndex === index;
 
-                                    return (
-                                        <button
-                                            key={index}
-                                            type="button"
-                                            onClick={() => setSelectedIndex(index)}
-                                            className={`
-              relative w-full py-3 px-6 text-center text-white
-              rounded-xl border-2 transition-all duration-200 border-transparent cursor-pointer
-            `}
-                                            style={{
-                                                background: isSelected ? color.selected : color.default,
-                                                // Если хочется ещё и бордер такого же цвета как фон (как у тебя на скрине)
-                                                borderColor: isSelected ? undefined : color.default,
-                                            }}
-                                        >
-                                            {color.name}
-                                        </button>
-                                    );
-                                })}
+                                        return (
+                                            <button
+                                                key={index}
+                                                type="button"
+                                                onClick={() => setSelectedIndex(index)}
+                                                className={`
+                relative w-full py-3 px-6 text-center text-white
+                rounded-xl border-2 transition-all duration-200 border-transparent cursor-pointer
+                `}
+                                                style={{
+                                                    background: isSelected ? color.selected : color.default,
+                                                    // Если хочется ещё и бордер такого же цвета как фон (как у тебя на скрине)
+                                                    borderColor: isSelected ? undefined : color.default,
+                                                }}
+                                            >
+                                                {color.name}
+                                            </button>
+                                        );
+                                    })}
+                                </div>
                             </div>
+                            <div className="relative">
+                                <Input handleClick={() => setSelectedDelay({ ...selectedDelay, clicked: !selectedDelay.clicked })} labelText={"Срок действия:"} placeholder={selectedDelay.value} style={"px-4 cursor-pointer"} readOnly={true} />
+                                <img src={selector} alt="scoreYellow" className="absolute top-13 right-4 z-300 w-4 h-4" />
+                                <AnimatePresence>
+                                    {selectedDelay.clicked && (
+                                        <m.div
+                                            initial={{ opacity: 0, y: -50 }}
+                                            animate={{ opacity: 1, y: 0 }}
+                                            exit={{ opacity: 0, y: -50 }}
+                                            transition={{ duration: 0.3 }}
+                                            className="absolute z-10 top-full left-0 w-full max-h-60 overflow-y-auto bg-gray-transparent backdrop-blur-2xl rounded-2xl border-2 border-modal">
+                                            {arrayDelay.map((version, index) => (
+                                                <div key={index} onClick={() => { setIsArrayVersionOpen(false); setSelectedDelay({ ...selectedDelay, value: version, clicked: false }) }} className="px-4 py-3 cursor-pointer bg-modal-bg hover:bg-gray-main duration-100">
+                                                    {version}
+                                                </div>
+                                            ))}
+                                        </m.div>
+                                    )}
+                                </AnimatePresence>
+                            </div>
+                            <div className="flex justify-center py-8">
+                                <p className="flex items-center gap-1"><span>Итог к оплате:</span><span className="pl-2"><img src={scoreYellow} alt="scoreYellow" className="w-4 h-4" /></span>1 020 <span className="text-sm line-through text-gray-main">1 244</span></p>
+                            </div>
+                            <Button type="submit" label={"Оплатить"} style={"bg-green-transparent w-full text-black"} />
                         </div>
-                        <div className="relative">
-                            <Input handleClick={() => setSelectedDelay({ ...selectedDelay, clicked: !selectedDelay.clicked })} labelText={"Срок действия:"} placeholder={selectedDelay.value} style={"px-4 cursor-pointer"} readOnly={true} />
-                            <img src={selector} alt="scoreYellow" className="absolute top-13 right-4 z-300 w-4 h-4" />
-                            <AnimatePresence>
-                                {selectedDelay.clicked && (
-                                    <m.div
-                                        initial={{ opacity: 0, y: -50 }}
-                                        animate={{ opacity: 1, y: 0 }}
-                                        exit={{ opacity: 0, y: -50 }}
-                                        transition={{ duration: 0.3 }}
-                                        className="absolute z-10 top-[110%] left-0 w-full max-h-60 overflow-y-auto bg-gray-transparent backdrop-blur-2xl rounded-2xl border-2 border-modal">
-                                        {arrayDelay.map((version, index) => (
-                                            <div key={index} onClick={() => { setIsArrayVersionOpen(false); setSelectedDelay({ ...selectedDelay, value: version, clicked: false }) }} className="px-4 py-3 cursor-pointer bg-modal-bg hover:bg-gray-main duration-100">
-                                                {version}
-                                            </div>
-                                        ))}
-                                    </m.div>
-                                )}
-                            </AnimatePresence>
-                        </div>
-                        <div className="flex justify-center py-8">
-                            <p className="flex items-center gap-1"><span>Итог к оплате:</span><span className="pl-2"><img src={scoreYellow} alt="scoreYellow" className="w-4 h-4" /></span>1 020 <span className="text-sm line-through text-gray-main">1 244</span></p>
-                        </div>
-                        <Button type="submit" label={"Оплатить"} style={"bg-green-transparent w-full text-black"} />
                     </m.form>
                 </m.div>
             }
--- src/components/ui/ModalPromo/ModalPromo.jsx
+++ src/components/ui/ModalPromo/ModalPromo.jsx
@@ -31,7 +31,7 @@ export default function ModalPromo({ isOpen, onClose }) {
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
                     transition={{ duration: 0.4 }}
-                    className="fixed inset-0 w-full h-full flex justify-center items-center bg-black-transparent px-4 py-20 md:py-0 z-500"
+                    className="fixed inset-0 w-full h-full flex justify-center items-center bg-black-transparent px-4 py-20 lg:py-0 z-500"
                 >
                     <m.form
                         onSubmit={(e) => handleSubmit(e)}
@@ -40,49 +40,51 @@ export default function ModalPromo({ isOpen, onClose }) {
                         animate={{ scale: 1, opacity: 1 }}
                         exit={{ scale: 0.8, opacity: 0 }}
                         transition={{ duration: 1, type: "spring" }}
-                        className="w-[450px] md:h-fit h-full overflow-y-scroll md:overscroll-none bg-modal-bg text-white relative p-5 box-border rounded-2xl backdrop-blur-2xl"
+                        className="w-[450px] md:h-fit h-full overflow-y-scroll md:overscroll-none bg-modal-bg text-white relative lg:p-5 box-border rounded-2xl backdrop-blur-2xl"
                     >
-                        <div className="relative pb-4">
+                        <div className="bg-modal-bg sticky top-0 pb-4 z-100 pt-5 lg:pt-0">
                             <h3 className="text-center font-semibold">Приобрести промо баллы</h3>
-                            <div className="absolute right-0 top-1/2 -translate-y-full">
+                            <div className="absolute right-4 lg:right-0 top-1/2 -translate-y-1/2">
                                 <img onClick={() => { onClose() }} className="cursor-pointer duration-100 hover:scale-130" src={cancel} alt="cancel" />
                             </div>
                         </div>
-                        <div className="relative">
-                            <div>
-                                <Input handleClick={() => setIsArrayVersionOpen(!isArrayVersionOpen)} labelText={"Выберите или добавьте сервер для раскрутки"} style={"cursor-pointer"} placeholder={selectedVersion} readOnly={true}/>
-                                <img src={selector} alt="selector" className="absolute right-4 md:top-14 top-20" />
+                        <div className="px-5 pb-5 lg:pb-0 lg:px-0">
+                            <div className="relative">
+                                <div>
+                                    <Input handleClick={() => setIsArrayVersionOpen(!isArrayVersionOpen)} labelText={"Выберите или добавьте сервер для раскрутки"} style={"cursor-pointer"} placeholder={selectedVersion} readOnly={true}/>
+                                    <img src={selector} alt="selector" className="absolute right-4 md:top-14 top-20" />
+                                </div>
+                                <AnimatePresence>
+                                    {isArrayVersionOpen && (
+                                        <m.div
+                                        initial={{opacity: 0, y: -50}}
+                                        animate={{opacity: 1, y: 0}}
+                                        exit={{opacity: 0, y: -50}}
+                                        transition={{duration: 0.3}}
+                                        className="absolute z-10 top-[110%] left-0 w-full max-h-60 overflow-y-auto bg-gray-transparent backdrop-blur-2xl rounded-2xl border-2 border-modal">
+                                            {arrayVersion.map((version, index) => (
+                                                    <div key={index} onClick={() => {setIsArrayVersionOpen(false); setSelectedVersion(version)}} className="px-4 py-3 cursor-pointer bg-modal-bg hover:bg-gray-main duration-100">
+                                                        {version}
+                                                    </div>
+                                                ))}
+                                        </m.div>
+                                    )}
+                                </AnimatePresence>
                             </div>
-                            <AnimatePresence>
-                                {isArrayVersionOpen && (
-                                    <m.div
-                                    initial={{opacity: 0, y: -50}}
-                                    animate={{opacity: 1, y: 0}}
-                                    exit={{opacity: 0, y: -50}}
-                                    transition={{duration: 0.3}}
-                                    className="absolute z-10 top-[110%] left-0 w-full max-h-60 overflow-y-auto bg-gray-transparent backdrop-blur-2xl rounded-2xl border-2 border-modal">
-                                        {arrayVersion.map((version, index) => (
-                                                <div key={index} onClick={() => {setIsArrayVersionOpen(false); setSelectedVersion(version)}} className="px-4 py-3 cursor-pointer bg-modal-bg hover:bg-gray-main duration-100">
-                                                    {version}
-                                                </div>
-                                            ))}
-                                    </m.div>
-                                )}
-                            </AnimatePresence>
+                            <div className="flex flex-col py-5 gap-3">
+                                {[["Топ 1", ["#FFC300", "#FFC30054"], sale35], ["Топ 2", ["#E1E2E1", "#FFFFFF1A"], sale25], ["Топ 3", ["#FF6200", "#FF62001A"], sale15], ["Топ 4", ["#00FFD4", "#1F201F"]], ["Топ 5", ["#00FFD4", "#1F201F"]]].map((item, index) => (
+                                    <PromoButton key={index} style={item[1]} title={item[0]} sale={item[2]}/>
+                                ))}
+                            </div>
+                            <div className="relative">
+                                <img src={scoreYellow} alt="scoreYellow" className="absolute top-13 left-4 z-300 w-4 h-4" />
+                                <Input value={1234} labelText={"Количество промо баллов:"} placeholder="0" style={"px-9"} readOnly={true}/>
+                            </div>
+                            <div className="flex justify-center py-8">
+                                <p className="flex items-center gap-1"><span>Итог к оплате:</span><span className="pl-2"><img src={scoreYellow} alt="scoreYellow" className="w-4 h-4"/></span>1 020 <span className="text-sm line-through text-gray-main">1 244</span></p>
+                            </div>
+                            <Button type="submit" label={"Оплатить"} style={"bg-green-transparent w-full text-black"}/>
                         </div>
-                        <div className="flex flex-col py-5 gap-3">
-                            {[["Топ 1", ["#FFC300", "#FFC30054"], sale35], ["Топ 2", ["#E1E2E1", "#FFFFFF1A"], sale25], ["Топ 3", ["#FF6200", "#FF62001A"], sale15], ["Топ 4", ["#00FFD4", "#1F201F"]], ["Топ 5", ["#00FFD4", "#1F201F"]]].map((item, index) => (
-                                <PromoButton key={index} style={item[1]} title={item[0]} sale={item[2]}/>
-                            ))}
-                        </div>
-                        <div className="relative">
-                            <img src={scoreYellow} alt="scoreYellow" className="absolute top-13 left-4 z-300 w-4 h-4" />
-                            <Input value={1234} labelText={"Количество промо баллов:"} placeholder="0" style={"px-9"} readOnly={true}/>
-                        </div>
-                        <div className="flex justify-center py-8">
-                            <p className="flex items-center gap-1"><span>Итог к оплате:</span><span className="pl-2"><img src={scoreYellow} alt="scoreYellow" className="w-4 h-4"/></span>1 020 <span className="text-sm line-through text-gray-main">1 244</span></p>
-                        </div>
-                        <Button type="submit" label={"Оплатить"} style={"bg-green-transparent w-full text-black"}/>
                     </m.form>
                 </m.div>
             }
