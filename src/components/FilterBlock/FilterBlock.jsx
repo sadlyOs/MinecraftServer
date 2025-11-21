6f7c75e9eee9cb47ff091c27aa36bbd0714da64e
sadlyOs <jukovkill@gmail.com>
2025-11-22 01:11:08 +0500

    edit hero section

--- src/components/FilterBlock/FilterBlock.jsx
+++ src/components/FilterBlock/FilterBlock.jsx
@@ -4,7 +4,7 @@ export default function FilterBlock({ title, tags, selected, onToggle, textCente
     return (
         <div className={`text-white pb-6 not-last:border-b not-last:border-b-white-light ${textCenter == "center" && 'text-center'}`}>
             <h3 className={`pb-3 text-xl`}>{title}</h3>
-            <div className={`flex flex-wrap gap-2 ${textCenter == "center" && 'justify-center'}`}>
+            <div className={`flex flex-wrap gap-1.5 ${textCenter == "center" && 'justify-center'}`}>
                 {tags.map((tag) => (
                     <FilterButton key={tag.value} tag={tag} onToggle={onToggle} selected={selected}/>
                 ))}
